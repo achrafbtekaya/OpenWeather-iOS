1b1b31c9fcae0c61e8595ff849c05328740f7a84
achrefBT <36794199+achrefBT@users.noreply.github.com>
2023-03-15 18:04:29 +0100

    update framework unit test bu adding new cases and change testing location

--- OpenWeather_app/OpenWeatherTests/OpenWeatherTests.swift
+++ OpenWeather_app/OpenWeatherTests/OpenWeatherTests.swift
@@ -7,30 +7,93 @@
 
 import XCTest
 @testable import OpenWeather
+@testable import OpenWeatherAPIs
 
 final class OpenWeatherTests: XCTestCase {
+    var openWeatherAPIs: OpenWeatherAPIs!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        openWeatherAPIs = OpenWeatherAPIs(apiKey: "de79c9827949fa4d24bcdee6a6408c55")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testFindCityCoordinates() {
+        let expectation = self.expectation(description: "City List")
+        
+        //1st Test : Correct Name
+        openWeatherAPIs.findCityCoordinates("Paris") { result in
+            XCTAssertNotNil(result.response())
+            XCTAssertNil(result.error())
+            XCTAssertNotNil(result.data())
+            
+            expectation.fulfill()
         }
+        
+        //2nd Test : Wrong Name
+        openWeatherAPIs.findCityCoordinates("Lorem epsum") { result in
+            XCTAssertNotNil(result.response())
+            XCTAssertNil(result.error())
+            XCTAssertNotNil(result.data())
+            if let errorCode = result.data()?["cod"] as? String {
+                XCTAssertEqual(errorCode, "404")
+            }
+
+            expectation.fulfill()
+        }
+        
+        //3rd Test : Empty Param
+        openWeatherAPIs.findCityCoordinates("") { result in
+            XCTAssertNotNil(result.response())
+            XCTAssertNil(result.error())
+            XCTAssertNotNil(result.data())
+            if let errorCode = result.data()?["cod"] as? String {
+                XCTAssertEqual(errorCode, "404")
+            }
+
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 1.0, handler: nil)
     }
+    
+    func testRetreiveWeatherData() {
+        let expectation = self.expectation(description: "City Weather")
+        
+        //1st Test : Correct Coordinates
+        openWeatherAPIs.retreiveWeatherData(51.5073219, -0.1276474) { result in
+            XCTAssertNotNil(result.response())
+            XCTAssertNil(result.error())
+            XCTAssertNotNil(result.data())
+            if let errorCode = result.data()?["cod"] as? String {
+                XCTAssertEqual(errorCode, "400")
+            }
+
+            expectation.fulfill()
+        }
+        
+        //2nd Test : Wrong Latitude
+        openWeatherAPIs.retreiveWeatherData(9999999999, -0.1276474) { result in
+            XCTAssertNotNil(result.response())
+            XCTAssertNil(result.error())
+            XCTAssertNotNil(result.data())
+            if let errorCode = result.data()?["cod"] as? String {
+                XCTAssertEqual(errorCode, "400")
+            }
+
+            expectation.fulfill()
+        }
+
+        //3rd Test : Wrong Longitude
+        openWeatherAPIs.retreiveWeatherData(51.5073219, 999999999) { result in
+            XCTAssertNotNil(result.response())
+            XCTAssertNil(result.error())
+            XCTAssertNotNil(result.data())
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+
 
 }
